66cfe660b74bc1a412b2822b04cb95d671e486e3
Sanjaysundar27 <sanjaysundar2727@gmail.com>
2026-08-20 18:45:12 +0530

    Form Submit

--- src/Function Components/MyForm.jsx
+++ src/Function Components/MyForm.jsx
@@ -4,14 +4,18 @@ import { useState } from "react"
 export default function MyForm() {
     const [name, setName] = useState("");
 
-    console.log("current State : ", name)
+    
+    function handleSubmit(){
+        console.log("form Submited");
+        console.log("current State : ", name);                
+    }
 
     return (
 
-        <form>
+        <form onSubmit={handleSubmit}>
 
             <label> Enter your name : <input type="text" onChange={(e) => { setName(e.target.value) }} /></label>
-
+        <input type="submit" value="Submit form"/>
         </form>
 
     );
