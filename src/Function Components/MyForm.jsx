3b15cd29c7a4bb386131d0cffbf1975115b09758
Sanjaysundar27 <sanjaysundar2727@gmail.com>
2026-08-24 14:26:09 +0530

    I simplify the code to handle th e mutli input

--- src/Function Components/MyForm.jsx
+++ src/Function Components/MyForm.jsx
@@ -1,39 +1,46 @@
-import { useState } from "react"
+    import { useState } from "react"
 
 
-export default function MyForm() {
-    //const [name, setName] = useState("");
+    export default function MyForm() {
+        //const [name, setName] = useState("");
 
-    const [input, setInput] = useState({});
+        const [input, setInput] = useState({ Phone: "+91", Email: "@gmail.com" });
 
-    function handleSubmit(e) {
-        e.preventDefault();//This is for donot refersh the browser after submiting the value in input box
-        console.log("form Submited");
-        // console.log("current State : ", name);
-        console.log("Current State", input);
-    }
+
+        function handleSubmit(e) {
+            e.preventDefault();//This is for donot refersh the browser after submiting the value in input box
+            console.log("form Submited");
+            // console.log("current State : ", name);
+            console.log("Current State", input);
+        }
 
     function handleChange(e) {
-        const name = e.target.name;
-        const value = e.target.value;
-        setInput((pre) => {
-            return {
-                ...pre,
-                [name]: value
-            }
-        });
+        const { name, value } = e.target;
+        setInput((prev) => ({
+            ...prev,
+            [name]: value
+        }));
     }
 
-    return (
 
-        <form onSubmit={handleSubmit}>
+        return (
 
-            <label> Enter your name : <input type="text" name="Name" onChange={handleChange} /></label><br />
-            <label> Enter your Email : <input type="email" name="Email" onChange={handleChange} /></label><br />
-            <label> Enter your Phone No : <input type="tel" name="Phone_no" onChange={handleChange} /></label><br />
-            <input type="submit" value="Submit form" />
+            <form onSubmit={handleSubmit}>
 
-        </form>
+                <label> Enter your name : <input type="text" name="Name" onChange={handleChange} /></label><br />
+                <label> Enter your Email : <input type="email" name="Email" onChange={handleChange} value={input.Email} /></label><br />
+                <label> Enter your Phone No : <input type="tel" name="Phone" onChange={handleChange} value={input.Phone} /></label><br />
 
-    );
-}
+                <label>Select your country <select name="country" onChange={handleChange} value={input.country}>
+                    <option value="">Select</option>
+                    <option value="Australia">Australia</option>
+                    <option value="India">India</option>
+                    <option value="United States">United States</option>
+                </select>
+                </label><br/>
+                <input type="submit" value="Submit form" />
+
+            </form>
+
+        );
+    }
